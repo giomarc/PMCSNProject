117d75fb23606ce270a2a866fd28f7e77a490005
simonedaniello <deliterjob@gmail.com>
2019-03-09 16:57:08 +0100

    CSVLogger

--- src/main/java/system/CSVlogger.java
+++ src/main/java/system/CSVlogger.java
@@ -18,31 +18,33 @@ public class CSVlogger {
             if (! directory.exists()){
                 directory.mkdir();
             }
-            BufferedWriter outRT = new BufferedWriter(new FileWriter("./LOGS/" + fileResponseTime, true));
-            BufferedWriter outX = new BufferedWriter(new FileWriter("./LOGS/" + fileThroughput, true));
-            BufferedWriter outEN = new BufferedWriter(new FileWriter("./LOGS/" + fileAVGjobs, true));
-
 
             File fileRT = new File("./LOGS/" + fileResponseTime);
             File fileX = new File("./LOGS/" + fileThroughput);
             File fileEN = new File("./LOGS/" + fileAVGjobs);
             if (fileRT.createNewFile()) {
+                BufferedWriter outRT = new BufferedWriter(new FileWriter("./LOGS/" + fileResponseTime, true));
                 System.out.println("File Response Time has been created.");
-                outRT.write("system general, cloud general, cloudlet general, " +
+                outRT.write("lambda1, lambda2, mu1, mu2, n, seed, " +
+                        "system general, cloud general, cloudlet general, " +
                         "system class 1, cloud class 1, cloudlet class 1 , " +
                         "system class 2, cloud class 2, cloudlet class 2");
                 outRT.flush();
             }
             if (fileX.createNewFile()) {
+                BufferedWriter outX = new BufferedWriter(new FileWriter("./LOGS/" + fileThroughput, true));
                 System.out.println("File Throughput has been created.");
-                outX.write("system general, cloud general, cloudlet general, " +
+                outX.write("lambda1, lambda2, mu1, mu2, n, seed, " +
+                        "system general, cloud general, cloudlet general, " +
                         "system class 1, cloud class 1, cloudlet class 1 , " +
                         "system class 2, cloud class 2, cloudlet class 2");
                 outX.flush();
             }
             if (fileEN.createNewFile()) {
+                BufferedWriter outEN = new BufferedWriter(new FileWriter("./LOGS/" + fileAVGjobs, true));
                 System.out.println("File AVG jobs has been created.");
-                outEN.write("system general, cloud general, cloudlet general, " +
+                outEN.write("lambda1, lambda2, mu1, mu2, n, seed, " +
+                        "system general, cloud general, cloudlet general, " +
                         "system class 1, cloud class 1, cloudlet class 1 , " +
                         "system class 2, cloud class 2, cloudlet class 2");
                 outEN.flush();

15b3f413744f03d16a4f616995f250909201063f
simonedaniello <deliterjob@gmail.com>
2019-04-13 16:42:48 +0200

    matlab files

---
+++ MATLAB/batchMeansPlot.m
@@ -0,0 +1,22 @@
+function batchMeansPlot
+digits(15);
+M = readmatrix('..\RESULT_OUTPUT\BatchMeans.csv');
+x = M(:, 1) + 1;
+y = M(:, 10);
+
+figure();
+bar(y);
+title('Cloudlet AVG population batch means');
+xlabel('ID');
+ylabel('AVG population');
+ylim([min(y)-0.01 max(y)+0.01]);
+yline(mean(y));
+yline(min(y));
+yline(max(y));
+
+dim = [.2 .5 .3 .3];
+txtMin = ['Min: ' num2str(min(y)) ', AVG: ' num2str(mean(y)) ', Max: ' num2str(max(y))];
+annotation('textbox',dim,'String',txtMin,'FitBoxToText','on');
+
+end
+
